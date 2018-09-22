bf8de8e10489aadde85d55c716adc630f875ea43
Ly, Jonathan <lyj47@uwosh.edu>
2018-09-22 16:23:00 -0500

    Starts work on Problem2 by adding the state for pouring out a 7 liter

--- Problem2.py
+++ Problem2.py
@@ -6,17 +6,8 @@ V1.0
 The state space consists of 3 states containing the water amount for each 7, 4, and 3 liter jugs.
 
 You shouldn't pour more than 4 liters of water into the 4 liter jug, and 3 liters for the 3 liter jug.
-
-
 """
 
-'''
-ehhhhhhh i'm sean and i use single quotes ehghhhhhhhhhhhhhhhh - Sean Mitchell
-
-P.S hello GT, i luv u <4. Speak your sweet Clean Code words to me. And giv wife pls. Best waifu. - Sean Mitchell
-
-P.S.S i'm the dude doin' the practi-cummmmmmmm
-'''
 from search import Problem, breadth_first_search,depth_first_search, iterative_deepening_search
 
 class Problem2(Problem):
@@ -24,12 +15,34 @@ class Problem2(Problem):
     # Seven liter, Four liter, Three liter
     S = 0; F = 1; T = 2
 
+
+    tempS = self.initial[S]; tempF = self.initial[F]; tempT = self.initial[T]
+
     def __init__(self, goal):
         self.initial = (7,0,0)
         self.goal = goal
 
     list = []
     def actions(self, state):
+        newState = (0, 0, 0)
+        #the amounts of add and subtract
+
+        # Pour as much as you can into the 7 liters from 3 liter
+        for x in range(state[S]):
+            if tempS == 0 OR tempT == 3:
+                break
+            tempS = tempS - 1
+            tempT = tempT + 1
+
+        newState = (tempS, state[F], tempT)
+        reset()
+        if(validate(newState)):
+            list.append(newState)
+        # Pour as much as you can into the 7 liters from 4 liter
+        newState = (state[S] + state[F], state[F] - state[F], state[T])
+        if(validate(newState)):
+            list.append(newState)
+
 
 
     def validate(self, state):
@@ -42,6 +55,11 @@ class Problem2(Problem):
             return False
 
         return True
+
+    def reset():
+        state[S] = self.initial[S]
+        state[F] = self.initial[F]
+        state[T] = self.initial[T]
 
 def main():
 
